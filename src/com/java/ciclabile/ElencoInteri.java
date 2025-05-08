d2efb889eab2b5e7b0c398d4f3bb9e58da79b0dd
alessiozaccato <alessiozaccato@gmail.com>
2025-05-08 17:54:17 +0200

    first commit

---
+++ src/com/java/ciclabile/ElencoInteri.java
@@ -0,0 +1,68 @@
+package com.java.ciclabile;
+
+public class ElencoInteri {
+
+    private int[] arrayInt;
+    private int counter = 0;
+
+    public ElencoInteri(int[] arrayIntParameter) {
+
+        this.arrayInt = arrayIntParameter;
+
+    }
+
+    //for the bonus
+    public ElencoInteri() {
+        this.arrayInt= new int[0];
+    }
+
+    public int[] getArrayInt() {
+        return this.arrayInt;
+    }
+
+    public void setArrayInt(int[] arrayInt) {
+        this.arrayInt = arrayInt;
+    }
+
+    public boolean hasAncoraElementi() {
+
+        if (arrayInt.length == 0) {
+
+            return false;
+
+        }
+
+        return this.counter < this.arrayInt.length;
+
+    }
+
+    public int getElementoSuccessivo() {
+
+        int nextInt = 0;
+
+        nextInt = arrayInt[counter];
+
+        counter++;
+
+        return nextInt;
+
+    }
+
+    public void addElemento(int element) {
+
+        int[] arrayClone = new int[arrayInt.length + 1];
+
+        for(int i=0; i< arrayInt.length; i++) {
+
+            arrayClone[i] = arrayInt[i];
+
+        }
+
+        arrayClone[arrayInt.length] = element;
+        arrayInt=arrayClone;
+
+    }
+
+
+
+}
---
+++ src/com/java/ciclabile/Main.java
@@ -0,0 +1,34 @@
+package com.java.ciclabile;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arrayInt = { 1, 2, 4, 6, 5 };
+
+        ElencoInteri integer = new ElencoInteri(arrayInt);
+
+        while (integer.hasAncoraElementi()) {
+
+            System.out.println(integer.getElementoSuccessivo());
+
+        }
+
+        ElencoInteri integerBonus = new ElencoInteri();
+        integerBonus.addElemento(2);
+        integerBonus.addElemento(3);
+
+        while (integerBonus.hasAncoraElementi()) {
+            System.out.println(integerBonus.getElementoSuccessivo());
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+}
